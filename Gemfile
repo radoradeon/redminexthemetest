923a6727431eb4d3e2cff65085a3289cc00724cd
radoradeon <radoslawhirek@gmail.com>
2022-02-18 18:44:05 +0100

    asd233dsd

--- Gemfile
+++
@@ -1,1 +0,0 @@
-gem 'redmine_extensions' unless Dir.exist?(File.expand_path('../../easyproject', __FILE__))
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-# Because of plugin deactivations
-if Redmine::Plugin.installed?(:easy_wbs)
-  resources :projects do
-    get 'easy_wbs', to: 'easy_wbs#index', as: 'easy_wbs_index'
-  end
-end
