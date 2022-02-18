3fcc4dda2eebd4317ecfaa1cfe8df6e529ad0c88
radoradeon <radoslawhirek@gmail.com>
2022-02-18 19:01:29 +0100

    d

--- lib/easy_wbs/easy_wbs.rb
+++
@@ -1,7 +0,0 @@
-module EasyWbs
-
-  def self.easy_extensions?
-    Redmine::Plugin.installed?(:easy_extensions)
-  end
-
-end
--- lib/easy_wbs/hooks.rb
+++
@@ -1,9 +0,0 @@
-module EasyWbs
-  class Hooks < Redmine::Hook::ViewListener
-
-    def helper_options_for_default_project_page(context={})
-      context[:default_pages] << 'easy_wbs' if context[:enabled_modules].include?('easy_wbs')
-    end
-
-  end
-end
