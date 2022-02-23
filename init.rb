88c7aa61c794767c20a4774ffbc23b4ead4dbf0c
Radosław Hirski <hirski.r@CITYPG.local>
2022-02-23 09:04:29 +0100

    ddd3

--- init.rb
+++
@@ -1,9 +0,0 @@
-require_dependency 'redmine_x_client/view_layouts_base_hook'
-
-Redmine::Plugin.register :redmine_x_client do
-  name 'RedmineX Client plugin'
-  author 'Ondrej Svejkovsky'
-  description 'Plugin for custom css and js'
-  version '1.0.0'
-  url 'https://redmine-x.com'
-end
--- lib/redmine_x_client/view_layouts_base_hook.rb
+++
@@ -1,9 +0,0 @@
-module RedmineXClient
-  class ViewLayoutsBaseHook < Redmine::Hook::ViewListener
-    # Adds SmartsUpp chat js to the base layout (i.e. to every page)
-    def view_layouts_base_html_head(context = {})
-      stylesheet_link_tag('client.css', plugin: :redmine_x_client) +
-      javascript_include_tag('client.js', plugin: :redmine_x_client)
-    end
-  end
-end
